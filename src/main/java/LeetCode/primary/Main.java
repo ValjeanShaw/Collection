56de17bfed5a5ef3fd9a37fd072b4968d57f5c8b
xiaoran <xiaoran@syswin.com>
2018-07-31 11:05:54 +0800

    旋转数组

--- src/main/java/LeetCode/primary/Main.java
+++ src/main/java/LeetCode/primary/Main.java
@@ -11,15 +11,15 @@ public class Main {
 
 //        int[] array = {7,1,4,5,3,6};
 //        int[] array = {1,2,3,4,5};
-        int[] array = {5,4,3,2,1};
+        int[] array = {5, 4, 3, 2, 1};
         Main main = new Main();
         int sum = main.maxProfit(array);
         System.out.println(sum);
     }
 
     /**
-     *从排序数组中删除重复项
-     *
+     * 从排序数组中删除重复项
+     * <p>
      * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/21/
      *
      * @param nums
@@ -42,7 +42,7 @@ public class Main {
 
     /**
      * 买卖股票的最佳时机
-     *
+     * <p>
      * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/description/
      *
      * @param prices
@@ -52,15 +52,15 @@ public class Main {
         int low = 0;
         int high = 0;
         int sum = 0;
-        while (low < (prices.length - 1) && (high < prices.length -1)) {
+        while (low < (prices.length - 1) && (high < prices.length - 1)) {
             if (prices[low] >= prices[low + 1]) {
-                low ++;
-            }else{
+                low++;
+            } else {
                 high = low;
                 while (high < prices.length - 1) {
-                    if(prices[high] <= prices[high+1]){
-                        high ++;
-                    }else{
+                    if (prices[high] <= prices[high + 1]) {
+                        high++;
+                    } else {
                         break;
                     }
                 }
@@ -71,4 +71,26 @@ public class Main {
 
         return sum;
     }
+
+    /**
+     * 旋转数组
+     * <p>
+     * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/23/
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int[] arrays = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arrays[(i + k) % len] = nums[i];
+        }
+        for (int i = 0; i < len; i++) {
+            nums[i] = arrays[i];
+        }
+    }
+
+    
 }
